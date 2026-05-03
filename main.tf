2213a9568065cc3b785425830c1268a52ea57de8
Imre Bodnár <bodnar.imre.85@gmail.com>
2026-05-04 00:13:57 +0200

    Configured AWS EC2 instance

--- main.tf
+++ main.tf
@@ -3,54 +3,80 @@ provider "aws" {
 }
 
 # VARIABLES
-variable "cidr_blocks" {
-    description = "CIDR blocks for VPC and subnets"
-    type = list(string)
-}
-variable "environment" {
-    description = "deployment environment"
-    default = "staging"
-    type = string
-}
-# Ehhez kell lennie egy TF_VAR_avail_zone nevű környezeti változónak a hoszt gépen.
-# variable avail_zone {}
+variable vpc_cidr_block {}
+variable subnet_cidr_block {}
+variable avail_zone {}
+variable env_prefix {}
+variable my_ip {}
 
-resource "aws_vpc" "development-vpc" {
-    cidr_block = var.cidr_blocks[0]
+# RESOURCES
+resource "aws_vpc" "myapp-vpc" {
+    cidr_block = var.vpc_cidr_block
+
     tags = {
-        Name: var.environment
+        Name: "${var.env_prefix}-vpc"
     }
 }
 
-resource "aws_subnet" "dev-subnet-1" {
-    vpc_id = aws_vpc.development-vpc.id
-    cidr_block = var.cidr_blocks[1]
-    availability_zone = "eu-north-1a"
+resource "aws_subnet" "myapp-subnet-1" {
+    vpc_id = aws_vpc.myapp-vpc.id
+    cidr_block = var.subnet_cidr_block
+    availability_zone = var.avail_zone
+
     tags = {
-        Name: "subnet-1-dev"
+        Name: "${var.env_prefix}-subnet-1"
     }
 }
 
-# OUTPUTS
-output "dev-vpc-id" {
-    value = aws_vpc.development-vpc.id
+resource "aws_internet_gateway" "myapp-igw" {
+    vpc_id = aws_vpc.myapp-vpc.id
+
+    tags = {
+        Name: "${var.env_prefix}-igw"
+    }
 }
-output "dev-subnet-id" {
-    value = aws_subnet.dev-subnet-1.id
+
+resource "aws_default_route_table" "main-rtb" {
+    default_route_table_id = aws_vpc.myapp-vpc.default_route_table_id
+
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.myapp-igw.id
+    }
+
+    tags = {
+        Name: "${var.env_prefix}-main-rtb"
+    }
 }
 
+resource "aws_security_group" "myapp-sg" {
+    name = "myapp-sg"
+    vpc_id = aws_vpc.myapp-vpc.id
+    description = "Allow SSH and HTTP traffic"
 
-# JUST SOME EXAMPLES
+    ingress {
+        from_port = 22
+        to_port = 22
+        protocol = "TCP"
+        cidr_blocks = [var.my_ip]
+    }
 
-# data "aws_vpc" "existing_vpc" {
-#     default = true
-# }
+    ingress {
+        from_port = 8080
+        to_port = 8080
+        protocol = "TCP"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
 
-# resource "aws_subnet" "dev-subnet-2" {
-#     vpc_id = data.aws_vpc.existing_vpc.id
-#     cidr_block = "172.31.48.0/20"
-#     availability_zone = "eu-north-1a"
-#     tags = {
-#         Name: "subnet-2-default"
-#     }
-# }
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+        prefix_list_ids = []
+    }
+
+    tags = {
+        Name: "${var.env_prefix}-sg"
+    }
+}
